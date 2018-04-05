024c6d60c8c436e9fa1de005eef187d8c8ca7c8c
Austin Edgington <atedgington@gmail.com>
2018-04-05 16:06:29 -0700

    testing

--- dnd_bot.py
+++ dnd_bot.py
@@ -21,79 +21,81 @@ MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 #define title_except() function for later use in search block
 #The wikia we're scraping cares very much about capitialization in its URLs, so we're going to title case search queries except for articles
 def titlecase(s):
-    return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
-                    lambda mo: mo.group(0)[0].upper() +
-                            mo.group(0)[1:].lower(),
-                    s)
+        return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
+                        lambda mo: mo.group(0)[0].upper() +
+                                mo.group(0)[1:].lower(),
+                        s)
 
 def title_except(s, exceptions):
-    word_list = re.split(' ', s)
-    final = [titlecase(word_list[0])]
-    for word in word_list[1:]:
+        word_list = re.split(' ', s)
+        final = [titlecase(word_list[0])]
+        for word in word_list[1:]:
         final.append(word if word in exceptions else titlecase(word))
-    return " ".join(final)
+        return " ".join(final)
 
 def title_dash(s):
-    dashCapList = re.split('-',s)
-    print(dashCapList)
-    final = [dashCapList[0].capitalize()]
-    for section in dashCapList[1:]:
+        dashCapList = re.split('-',s)
+        print(dashCapList)
+        final = [dashCapList[0].capitalize()]
+        for section in dashCapList[1:]:
         final.append(section.capitalize())
-    return "-".join(final)
+        return "-".join(final)
 
 articles = ['a', 'an', 'of', 'the', 'is', 'with', 'into', 'and', 'on']
 
 def parse_bot_commands(slack_events):
-    """
+        """
         Parses a list of events coming from the Slack RTM API to find bot commands.
         If a bot command is found, this function returns a tuple of command and channel.
         If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
+        """
+        for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == bot_id:
+                user_id, message = parse_direct_mention(event["text"])
+                if user_id == bot_id:
                 return message, event["channel"]
-    return None, None
+        return None, None
 
 def parse_direct_mention(message_text):
-    """
+        """
         Finds a direct mention (a mention that is at the beginning) in message text
         and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+        """
+        matches = re.search(MENTION_REGEX, message_text)
+        # the first group contains the username, the second group contains the remaining message
+        return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def handle_command(command, channel):
-    """
+        """
         Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+        """
+        # Default response is help text for the user
+        default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
-    if 'do' in str(command.lower()):
+        # Finds and executes the given command, filling in response
+        response = None
+        # This is where you start to implement more commands!
+        if 'do' in str(command.lower()):
         response = "Sure...write some more code then I can do that!"
 
+
+
     # Sends the response back to the channel
-    slack_client.api_call(
+        slack_client.api_call(
         "chat.postMessage",
         channel=channel,
         text=response or default_response
-    )
+        )
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
+        if slack_client.rtm_connect(with_team_state=False):
         print("Slack Bot connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         bot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
-            command, channel = parse_bot_commands(slack_client.rtm_read())
-            if command:
+                command, channel = parse_bot_commands(slack_client.rtm_read())
+                if command:
                 handle_command(command, channel)
-            time.sleep(RTM_READ_DELAY)
-    else:
+                time.sleep(RTM_READ_DELAY)
+        else:
         print("Connection failed. Exception traceback printed above.")
