f8b07c72d60f4fcf64e9ebac4de2f36d013f5448
Austin Edgington <atedgington@gmail.com>
2018-04-09 16:11:35 -0700

    put in a print line so i can see syntax of slack output

--- dnd_bot.py
+++ dnd_bot.py
@@ -42,6 +42,7 @@ def title_dash(s):
         return "-".join(final)
 
 articles = ['a', 'an', 'of', 'the', 'is', 'with', 'into', 'and', 'on']
+keywords = ['weed', 'happy doggo']
 
 def parse_bot_commands(slack_events):
         """
@@ -51,9 +52,11 @@ def parse_bot_commands(slack_events):
         """
         for event in slack_events:
                 if event["type"] == "message" and not "subtype" in event:
+                        print(event["text"])
                         user_id, message = parse_direct_mention(event["text"])
                         if user_id == bot_id:
                                 return message, event["channel"]
+                
         return None, None
 
 def parse_direct_mention(message_text):
@@ -170,7 +173,7 @@ def handle_command(command, channel):
 
         if "happy doggo" in str(command.lower()):
                 response = "https://media.giphy.com/media/1Ju5mGZlWAqek/giphy.gif"
-                
+
         if "weed" in str(command.lower()):
                 response = ":weed:"
         #Lets keep the simple, one-off shitposting lines between these blocks - BOTTOM
