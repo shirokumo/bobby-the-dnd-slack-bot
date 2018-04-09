79ff95de0574e39a59f2720571189f44bea788ec
Austin Edgington <atedgington@gmail.com>
2018-04-09 15:29:17 -0700

    testing bobbys use of emojis

--- dnd_bot.py
+++ dnd_bot.py
@@ -170,6 +170,9 @@ def handle_command(command, channel):
 
         if "happy doggo" in str(command.lower()):
                 response = "https://media.giphy.com/media/1Ju5mGZlWAqek/giphy.gif"
+                
+        if "weed" in str(command.lower()):
+                response = ":weed:"
         #Lets keep the simple, one-off shitposting lines between these blocks - BOTTOM
 
 # Sends the response back to the channel
